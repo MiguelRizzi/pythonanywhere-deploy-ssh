5dbb60ed9a1c1a2243227347f134907c62c613a6
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2025-06-19 23:54:06 -0300

    asd

--- deployment/main.py
+++ deployment/main.py
@@ -3,14 +3,18 @@ from deployment.ssh import SSHConnector
 from deployment.manager import DeploymentManager
 
 
+
 def main():
-    if len(sys.argv) != 7:
-        print("Usage: main.py <ssh_host> <username> <ssh_key> <workdir> <venvdir> <wsgi>")
+    if len(sys.argv) != 8:
+        print("Usage: main.py <ssh_host> <username> <password> <ssh_key> <workdir> <venvdir> <wsgi>")
         sys.exit(1)
 
     ssh_host, username, password, ssh_key, workdir, venvdir, wsgi = sys.argv[1:]
 
-    ssh = SSHConnector(ssh_host, username, password, ssh_key)
+    # En caso de que no uses password, pásalo vacío: ""
+    # En caso de que no uses ssh_key, pásalo vacío: ""
+
+    ssh = SSHConnector(ssh_host, username, password if password else None, ssh_key if ssh_key else None)
     ssh.connect()
 
     manager = DeploymentManager(ssh, workdir, venvdir, wsgi)
@@ -20,5 +24,6 @@ def main():
         ssh.close()
 
 
+        
 if __name__ == "__main__":
     main()
